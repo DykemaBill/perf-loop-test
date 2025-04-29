c3166c8646e79b3e8296dcddc01974f46311feba
Dykema, Bill <github@dykema.org>
2025-04-29 09:50:58 -0500

    Done

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,25 @@ use os_info; // Operating system info
 use std::time::Instant; // Date/Time
 
 fn main() {
+    // Get the OS
     let operating_sys_info = os_info::get();
+    // Get the start time
     let now = Instant::now();
     
+    // Loop just for timed comparison
     loop_now();
 
+    // Gather times
     let elapsed_time = now.elapsed();
-
-    let elapsed_nanoseconds = elapsed_time.as_nanos();
+    let elapsed_secdecimals = elapsed_time.as_millis() % 1000;
     let elapsed_seconds = elapsed_time.as_secs() % 60;
     let elapsed_minutes = (elapsed_time.as_secs() / 60) % 60;
     let elapsed_hours = (elapsed_time.as_secs() / 60) / 60;
 
+    // Show the results
     println!("Run time for Rust on {} is: {:02}:{:02}:{:02}.{}",
-        operating_sys_info, elapsed_hours, elapsed_minutes, elapsed_seconds, elapsed_nanoseconds);
+        operating_sys_info, elapsed_hours, elapsed_minutes,
+        elapsed_seconds, elapsed_secdecimals);
 }
 
 fn loop_now() {
